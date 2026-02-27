070560607253734e54da9e9a7f0c8551a908289d
Oscar Cuevas <ocl816@alumnos.unican.es>
2026-02-27 12:53:18 +0100

    Version Inicial

---
+++ Practica2/seguros_bussiness/src/main/java/es/unican/is2/GestionSeguros.java
@@ -0,0 +1,39 @@
+package es.unican.is2;
+
+public class GestionSeguros {
+    
+    /* Comentadas ya que al no implementar la libreria daria error de compilacion */
+    // ClientesDAO clientesDao = new ClientesDAO()
+    // SegurosDAO segurosDao = new SegurosDAO()
+    
+
+    public void nuevoCliente() {
+
+    }
+
+    public void bajaCliente() {
+
+    }
+
+    public void nuevoSeguro() {
+
+    }
+
+    public void bajaSeguro() {
+
+    }
+
+    public void consultaCliente() {
+
+    }
+
+    public void consultaSeguro() {
+
+    }
+
+    public void anhadirConductorAdicional() {
+
+    }
+
+
+}
