352e9ec0eb478a62de5bb4ca9747a9f2a13801d8
Oscar Cuevas <ocl816@alumnos.unican.es>
2026-05-14 12:18:03 +0200

    Medidas añadidas a gestionTransportees.java

--- Practica6/gestion_transportes/src/main/java/es/unican/is2/gestionTransportes.java
+++ Practica6/gestion_transportes/src/main/java/es/unican/is2/gestionTransportes.java
@@ -3,25 +3,32 @@ package es.unican.is2;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+	WMC = 3 + 2 + 1 = 6
+	WMCn = 6 / 3 = 2.0 
+*/
 public class gestionTransportes {
 
 	private ArrayList<Conductor> cs = new ArrayList<Conductor>();
 	
-	public Conductor buscaConductor(String DNI) {		
-		for(Conductor c: cs) 
-			if (c.dni().equals(DNI))
+	// CC = 3 || CCog = 3
+	public Conductor buscaConductor(String DNI) { //CC + 1	
+		for(Conductor c: cs) // CC + 1 || CCog + 1
+			if (c.dni().equals(DNI)) // CC + 1  || CCog + 2 (por nivel de anidamiento)
 				return c;
 		
 		return null;
 	}
 	
-	public boolean anhadeConductor(String dni, String nombre, String apellido1, String apellido2, String direccion) {
-		if (buscaConductor(dni) != null)
+	//CC = 2 || CCog = 1
+	public boolean anhadeConductor(String dni, String nombre, String apellido1, String apellido2, String direccion) { // CC + 1
+		if (buscaConductor(dni) != null) // CC + 1 || CCog + 1
 			return false;
 		cs.add(new Conductor(dni, nombre, apellido1, apellido2,direccion));
 		return true;
 	}
 
+	//CC = 1 || CCog = 0
 	public List<Conductor> conductores() {
 		return cs;
 	}
