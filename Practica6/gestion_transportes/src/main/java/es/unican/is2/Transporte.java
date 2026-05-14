8f49e7228c9796fbf5c297a57e983f0acc6d2ec0
Oscar Cuevas <ocl816@alumnos.unican.es>
2026-05-14 12:32:36 +0200

    Añadidas metricas a Transporte.java

--- Practica6/gestion_transportes/src/main/java/es/unican/is2/Transporte.java
+++ Practica6/gestion_transportes/src/main/java/es/unican/is2/Transporte.java
@@ -1,5 +1,15 @@
 package es.unican.is2;
 
+/*
+	WMC = 9 (5 + 1 + 1 + 1 + 1)
+ 	WMCn = 9 / 5 = 1.8
+ 	CCog = 3 (3 + 0 + 0 + 0 + 0)
+ 	CCogn = 3 / 5 = 0.6
+ 	CBO = 2 (Clases acopladas: CategoriaTransporte, IllegalArgumentException) 
+ 	DIT = 1 (Hereda de Object)
+ 	NOC = 0 (No tiene subclases)
+*/
+
 /* Clase que representa un transporte realizado por un conductor */
 public class Transporte {
 	
@@ -16,32 +26,38 @@ public class Transporte {
 	 * representa el numero de personas, en caso de ser de tipo Mercancias 
 	 * representa las toneladas
 	 */ 
-	public Transporte(double horas, CategoriaTransporte cat, int valor) throws IllegalArgumentException {
-		if (horas <= 0 || valor <= 0 || cat == null) {
+
+	//CC = 5 || CCog = 3
+	public Transporte(double horas, CategoriaTransporte cat, int valor) throws IllegalArgumentException { //CC + 1
+		if (horas <= 0 || valor <= 0 || cat == null) { // CC + 1 + 2 || Ccog + 1 + 1
 			throw new IllegalArgumentException();
 		}
 		this.horas = horas;
 		this.cat = cat;
-		if (cat.equals(CategoriaTransporte.Personas)) {
+		if (cat.equals(CategoriaTransporte.Personas)) { //CC + 1 || Ccog + 1
 			this.personas = valor;
 		} else  {
 			this.ton = valor;
 		}
 	}
 	
-	public double horas() {
+	// CC = 1 || CCog = 0
+	public double horas() { //CC + 1
 		return horas;
 	}
-
-	public CategoriaTransporte categoria() {
+	
+	// CC = 1 || CCog = 0
+	public CategoriaTransporte categoria() { //CC + 1
 		return cat;
 	}
 
-	public int ton() {
+	// CC = 1 || CCog = 0
+	public int ton() { //CC + 1
 		return ton;
 	}
 
-	public int getPersonas() {
+	// CC = 1 || CCog = 0
+	public int getPersonas() { //CC + 1
 		return personas;
 	}
 	
